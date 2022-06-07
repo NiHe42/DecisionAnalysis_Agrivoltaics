cb43190ad78571aa838548d246cb0375d15f211a
JNBauer <julian.bauer@uni-bonn.de>
2022-06-08 00:11:51 +0200

    continued with extended model

--- decisionSupport.R
+++ decisionSupport.R
@@ -2,6 +2,7 @@ install.packages("decisionSupport")
 library(decisionSupport)
 
 
+
 input_estimates <- read.csv("data/av_estimates_new0602.csv", header = TRUE, sep =",")
 input_estimates
 
@@ -32,9 +33,13 @@ model_functionv1 <- function(){
 
 model_function <- function(){
   
+  av_int_execution_period <- rep(0,n_years)
+  
   # ex-ante risks
-  av_int_event_no_involvement_by_population <- chance_event(av_int_no_involvement_by_population, 1, 0, n = 1)
-  av_int_event_defect_photovoltaic_panels <- chance_event(av_int_event_photovoltaic_panels, 1, 0, n = 1)
+  av_int_event_no_involvement_by_population <-
+                chance_event(av_int_no_involvement_by_population, 1, 0, n = 1)
+  av_int_event_defect_photovoltaic_panels <-
+                chance_event(av_int_event_photovoltaic_panels, 1, 0, n = 1)
   
   # crop benefits VV
   av_crop_yield <-  vv(av_crop_ha, vv_var, n_years) *
@@ -51,41 +56,81 @@ model_function <- function(){
     if (decision_av_int)
     {
       decision_av_int_planning <- TRUE
-      decision_av_int_cost <- TRUE
+      decision_av_int_setup <- TRUE
       decision_av_int_execution <- TRUE
     }
     else
     {
 
       decision_av_int_planning <- FALSE
-      decision_av_int_cost <- FALSE
+      decision_av_int_setup <- FALSE
       decision_av_int_execution <- FALSE
     }
     
     if (av_int_event_no_involvement_by_population)
     {
-      decision_av_int_cost <- FALSE
+      decision_av_int_setup <- FALSE
       decision_av_int_execution <- FALSE
     }
       
   # cost calculation
-    if (decision_av_int_cost)
+    #calculation planning costs
+    if (decision_av_int_planning)
     {
-      
-      
+      av_int_planning <- av_int_cost_search_panels + av_int_cost_search_location
     }
-      
+    else
+    {
+      av_int_planning <- 0
+    }
     
+    #calculation setup costs   
+    if (decision_av_int_setup)
+    {
+      av_int_setup <- av_int_cost_photovoltaic_panels +
+                      av_int_cost_ground_preparation +
+                      av_int_cost_installation +
+                      av_int_cost_training
+    }
+    else
+    {
+      av_int_setup <- 0
+    }
     
+    #calculation execution/maintenance costs   
+    if (decision_av_int_execution)
+    {
+      av_int_execution <- av_int_execution_period + 
+                          vv(av_int_cost_reparation, vv_var, n_years) -
+                          vv(av_int_benefit_shade, vv_var, n_years)
+    }
+ 
     
+    av_int_cost <- av_int_execution
+    av_int_cost[1] <- av_int_cost[1] + av_int_setup + av_int_planning
     
+    if(decision_av_int)
+    {
+      total_benefits <- av_crop_yield + av_energy_yield
+    }
     
+    else
+    {
+      total_benefits <- av_crop_yield
+    }
     
+    if (decision_av_int)
+    {
+      net_benefits <- total_benefits + av_int_cost
+      result_int <- net_benefits
+    }
     
-    
-    
-    
-    
+    if (!decision_av_int)
+    {
+      net_benefits <- total_benefits + av_int_cost
+      result_int <- net_benefits
+    }   
+
   }
   
   
